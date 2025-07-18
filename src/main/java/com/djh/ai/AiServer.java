f39136562ddae38f6db06f68454b930ca7e1cd31
jiahaodai <418314316@qq.com>
2025-07-18 21:23:18 +0800

    spring-constructure

---
+++ src/main/java/com/djh/ai/AiServer.java
@@ -0,0 +1,15 @@
+package com.djh.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiServer {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiServer.class, args);
+
+    }
+
+}
